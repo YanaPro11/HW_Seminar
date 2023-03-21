36101ef9f61d72ef736583a8ef56a90ed56caa54
Yana Prokopova <112731047+YanaPro11@users.noreply.github.com>
2023-03-21 19:45:32 +0300

    Add files via upload

---
+++ ДЗ Семинар 1.py
@@ -0,0 +1,40 @@
+# Задача 2
+# x = 100
+# x= int(input("введите трехзначное число:"))
+# x3 = x % 10
+# x2 = int(x / 10 % 10)
+# x1 = int(x/100)
+# print (x1+x2+x3)
+
+#Задача 4
+# s=int(input("Введите количество журавликов:"))
+# kate=int((s/3)*2)
+# petya= int((kate/2)/2)
+# sergei= petya
+# print(petya,kate,sergei)
+
+#Задача 6
+# x=int(input("Введите номер билета:"))
+# x6=x%10
+# x5=x%100//10
+# x4=x%1000//100
+# x3=x%10000//1000
+# x2=x%100000//10000
+# x1=x//100000
+# if x1+x2+x3==x4+x5+x6:
+#     print("yes")
+# else:
+#     print("no")
+
+#Задача 8
+# n= int(input("Введите n долек:"))
+# m= int(input("Введите m долек:"))
+# k= int(input("Введите k долек:"))
+# if k<n*m:
+#     if k%n==0 or k%m==0:
+#         print("yes")
+#     else:
+#         print("no")
+# else:
+#     print("no")
+
