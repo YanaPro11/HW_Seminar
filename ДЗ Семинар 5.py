27d1d980a87aa698ca80e7108b4c0f13e49d547b
Yana Prokopova <112731047+YanaPro11@users.noreply.github.com>
2023-04-03 12:07:25 +0300

    Add files via upload

---
+++ ДЗ Семинар 5.py
@@ -0,0 +1,21 @@
+# Задача 26
+# def exponent(a, b):
+#      if b==0:
+#          return 1
+#      return a*exponent(a,b-1)
+# a = int(input("Введите число:"))
+# b = int(input("Введите степень:"))
+# print(exponent(a, b))
+
+# Задача 28
+# def sum_rec(a, b):
+#     if b == 0:
+#         return a
+#     elif a == 0:
+#         return b
+#     return 1 + sum_rec(a, b - 1)
+#
+#
+# a = int(input("Введите первое число:"))
+# b = int(input("Введите второе число:"))
+# print(sum_rec(a, b))
