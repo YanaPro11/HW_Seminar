26c5f13552dea4ef4ed2e82a6ed6b30948fe92ac
Yana Prokopova <112731047+YanaPro11@users.noreply.github.com>
2023-04-03 12:29:56 +0300

    Add files via upload

---
+++ ДЗ Семинар 6.py
@@ -0,0 +1,21 @@
+# Задача 30
+# a1 = int(input("Введите первый элемент:"))
+# d = int(input("Введите разность элементов:"))
+# n = int(input("Введите количество элементов:"))
+# arif_prog = []
+# for i in range(n):
+#     arif_prog.append(a1 + ((i +1)-1) * d)
+# print(arif_prog)
+
+# Задача 32
+# numbers = []
+# min_list = int(input("Введите первое число списка:"))
+# max_list = int(input("Введите последнее число списка:"))
+# for i in range(min_list, max_list + 1):
+#     numbers.append(i)
+# print(numbers)
+# min_number = int(input("Введите минимум:"))
+# max_number = int(input("Введите максимум:"))
+# for i in range(len(numbers)):
+#     if numbers[i] >= min_number and numbers[i] <= max_number:
+#         print(i)
